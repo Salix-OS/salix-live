a62277f4c7912337be07713ee1c29a5afe08495d
Cyrille Pontvieux <jrd@enialis.net>
2012-09-13 01:39:17 +0200

    Version for 13.37

--- .getsalt-scripts.sh
+++ .getsalt-scripts.sh
@@ -4,7 +4,7 @@
 # Used to download the correct version of SaLT Scripts and SaLT.
 
 cd "$(dirname "$0")"
-SALT_SCRIPTS_VER='master'
+SALT_SCRIPTS_VER='tags/0.2.0'
 SALT_SCRIPTS_URL='git://github.com/jrd/SaLT-scripts.git'
 if [ -d salt-scripts ]; then
   rm -rf salt-scripts || echo "salt-scripts directory cannot be removed, check permissions" >&2
